94a4e4f182515075f6a0e33c8abd07c6b33348e6
Mittal <mittalhalarnekar@gmail.com>
2026-06-11 15:49:07 +0530

    Created Routes for login&signup page

--- frontend/travel-story-app/src/App.jsx
+++ frontend/travel-story-app/src/App.jsx
@@ -1,7 +1,22 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import React from "react";
 
+import login from "./pages/Auth/login";
+import signup from "./pages/Auth/signup";
+import home from "./pages/Home/home";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/home" element={<home />} />
+          <Route path="/login" element={<login />} />
+          <Route path="/signup" element={<signup />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 };
 
 export default App;
