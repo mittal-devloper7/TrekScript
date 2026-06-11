2b56a7c260701540286e60bc29b1612d5d7aa9fc
Mittal <mittalhalarnekar@gmail.com>
2026-06-11 16:10:15 +0530

    Fixed Routes of Login & SignUP pages

--- frontend/travel-story-app/src/App.jsx
+++ frontend/travel-story-app/src/App.jsx
@@ -1,18 +1,18 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import React from "react";
 
-import login from "./pages/Auth/login";
-import signup from "./pages/Auth/signup";
-import home from "./pages/Home/home";
+import Login from "./pages/Auth/login";
+import Signup from "./pages/Auth/signup";
+import Home from "./pages/Home/home";
 
 const App = () => {
   return (
     <div>
       <Router>
         <Routes>
-          <Route path="/home" element={<home />} />
-          <Route path="/login" element={<login />} />
-          <Route path="/signup" element={<signup />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
         </Routes>
       </Router>
     </div>
