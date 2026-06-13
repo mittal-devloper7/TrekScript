9d4b581452c2544d7445c5f689fb8dc025b5bf0e
Mittal <mittalhalarnekar@gmail.com>
2026-06-13 22:22:55 +0530

    MongoDB Atlas configuration

--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,24 @@
+require("dotenv").config();
+const config = require("./config.json");
 const bcrypt = require("bcrypt");
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
 
+mongoose.connect(config.connectionString);
+
 const app = express();
 
 app.use(express.json());
 app.use(cors({ origin: "*" }));
 
-app.get("/", (req, res) => {
+app.get("/create-account", (req, res) => {
   return res.status(200).send("Hello World");
 });
 
 app.listen(5000, () => {
   console.log("Server is running on port 5000");
 });
+
+module.exports = app;
